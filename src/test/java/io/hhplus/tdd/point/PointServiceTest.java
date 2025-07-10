9346672be7168b041274d677abb1c788cd231ba1
rigollet <rigollet153@gmail.com>
2025-07-10 23:47:13 +0900

    포인트 조회 시의 다양한 테스트 코드 작성

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -21,4 +21,43 @@ public class PointServiceTest {
         // Then
         assertEquals(1000, result);
     }
+
+    @Test
+    void 존재하지_않는_사용자_조회() {
+        // Given
+        long userId = 1L;
+        UserPointTable table = new UserPointTable();
+        table.insertOrUpdate(userId, 1000);
+
+        PointService service = new PointService(table);
+
+        // When Then
+        assertThrows(NullPointerException.class, () -> service.getPoint(999L));
+    }
+
+    @Test
+    void 포인트가_0인_사용자_조회() {
+        // Given
+        long userId = 2L;
+        UserPointTable table = new UserPointTable();
+        table.insertOrUpdate(userId, 0);
+
+        PointService service = new PointService(table);
+
+        // When Then
+        assertEquals(0, service.getPoint(2L));
+    }
+
+    @Test
+    void 잘못된_ID() {
+        // Given
+        long userId = 1L;
+        UserPointTable table = new UserPointTable();
+        table.insertOrUpdate(userId, 1000);
+
+        PointService service = new PointService(table);
+
+        // When Then
+        assertThrows(IllegalArgumentException.class, () -> service.getPoint(-1L));
+    }
 }
